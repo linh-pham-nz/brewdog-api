b51b1df12ccc1fbf59e461da50d48faeac5c891f
linh-pham-nz <linh.pham.nz@gmail.com>
2019-10-24 15:43:43 +1300

    WIP - styling, rows and columns added for responsiveness

--- client/components/ChosenBeer.jsx
+++ client/components/ChosenBeer.jsx
@@ -25,18 +25,23 @@ class ChosenBeer extends React.Component {
         return (
             <React.Fragment>
                 <h4>{this.state.beer.name}</h4>
-                <p>Tagline: {this.state.beer.tagline}</p>
-                <p>Description: {this.state.beer.description}</p>
-                <p>ABV: {this.state.beer.abv}</p>
-                <p>First brewed: {this.state.beer.first_brewed}</p>
-                <p>Food pairing: {this.state.beer.food_pairing}</p>
-                {/* <ul>
-                    {this.state.beer.food_pairing.map(foods => (
-                        <li key={foods}>{foods}</li>
-                    ))}
-                </ul> */}
-                <img src={this.state.beer.image_url}></img>
-
+                <div className="row">
+                    <div className="col-xl-6 col-sm-6">
+                        <p>Tagline: {this.state.beer.tagline}</p>
+                        <p>Description: {this.state.beer.description}</p>
+                        <p>ABV: {this.state.beer.abv}</p>
+                        <p>First brewed: {this.state.beer.first_brewed}</p>
+                        <p>Food pairing: {this.state.beer.food_pairing}</p>
+                        {/* <ul>
+                            {this.state.beer.food_pairing.map(foods => (
+                                <li key={foods}>{foods}</li>
+                            ))}
+                        </ul> */}
+                    </div>
+                    <div className="col-xl-6 col-sm-6">
+                        <img src={this.state.beer.image_url}></img>
+                    </div>
+                </div>
             </React.Fragment>
         )
     }
