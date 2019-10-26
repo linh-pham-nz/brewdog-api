a60a7f9c4a739a6a81d3d57d0c4dcab510977003
linh-pham-nz <linh.pham.nz@gmail.com>
2019-10-26 15:42:16 +1300

    creat link to Home page

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
         <React.Fragment>
             <h1>BREWDOG BEER LIST</h1>
             <ul>
+                <li><Link to="/">Home</Link></li>
                 <li><Link to="/filters">Filters</Link></li>
             </ul>
         </React.Fragment>
