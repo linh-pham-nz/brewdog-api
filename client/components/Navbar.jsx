a3ecb1bc921ac085f37e5c82bd714b4b1b4cf386
linh-pham-nz <linh.pham.nz@gmail.com>
2019-11-06 17:23:41 +1300

    remove comments, minor fixes

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-// import * from '../../server/public/BrewDogLogo.png'
-
 const Navbar = () => {
     return (
         <React.Fragment>
