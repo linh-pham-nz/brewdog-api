86e0d0fa1db4238f36e5d16082ee28d8543b484d
linh-pham-nz <linh.pham.nz@gmail.com>
2019-11-06 13:19:27 +1300

    WIP - navbar continue styling

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -7,13 +7,15 @@ const Navbar = () => {
     return (
         <React.Fragment>
             <nav className="navbar navbar-expand-md navbar-light bg-light">
-                <div className="d-flex logo">
-                    <Link to="/"><img className="logo-image" src="BrewDogLogo.png"></img><h1>BREWDOG BEERS</h1></Link>
-                </div>
-                <div className="navbar-nav">
-                    <Link className="nav-item nav-link active" to="/">Home</Link>
-                    <Link className="nav-item nav-link" to="/filters">Filters</Link>
-                    <Link className="nav-item nav-link" to="/beers">Beers</Link>
+                <div className="container-fluid">
+                    <div className="d-flex logo">
+                        <Link to="/"><img className="logo-image" src="BrewDogLogo.png"></img><h1>BREWDOG BEERS</h1></Link>
+                    </div>
+                    <div className="navbar-nav">
+                        <Link className="nav-item nav-link active" to="/">Home</Link>
+                        <Link className="nav-item nav-link" to="/filters">Filters</Link>
+                        <Link className="nav-item nav-link" to="/beers">Beers</Link>
+                    </div>
                 </div>
             </nav>
         </React.Fragment>
