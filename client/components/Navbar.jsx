cebb426c705a125c89adc1330bc5b8952af8709a
linh-pham-nz <linh.pham.nz@gmail.com>
2019-11-15 13:37:16 +1300

    WIP - navbar - divide content into rows

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -6,21 +6,25 @@ const Navbar = () => {
         <React.Fragment>
             <nav className="navbar navbar-expand-md navbar-light bg-light">
                 <div className="container-fluid">
-                    <div className="d-flex logo">
-                        <Link to="/">
-                            <img 
-                                className="logo-image" 
-                                src="BrewDogLogo.png">
-                            </img>
-                            <h1>BREWDOG BEERS</h1>
-                        </Link>
-                    </div>
-                    <br></br>
-                    <div className="navbar-nav row" id="nav-row">
-                        <Link className="nav-item nav-link active" to="/">Home</Link>
-                        <Link className="nav-item nav-link" to="/filters">Filters</Link>
-                        <Link className="nav-item nav-link" to="/beers">Beers</Link>
-                        <Link className="nav-item nav-link" to="/random-beer">Random Beer</Link>
+                    <div className="row">
+                        <div className="d-flex logo">
+                            <Link to="/">
+                                <img 
+                                    className="logo-image" 
+                                    src="BrewDogLogo.png">
+                                </img>
+                                <h1>BREWDOG BEERS</h1>
+                            </Link>
+                        </div>
+                        <br></br>
+                        </div>
+                    <div className="row">
+                        <div className="navbar-nav row" id="nav-row">
+                            <Link className="nav-item nav-link active" to="/">Home</Link>
+                            <Link className="nav-item nav-link" to="/filters">Filters</Link>
+                            <Link className="nav-item nav-link" to="/beers">Beers</Link>
+                            <Link className="nav-item nav-link" to="/random-beer">Random Beer</Link>
+                        </div>
                     </div>
                 </div>
             </nav>
