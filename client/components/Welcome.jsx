6b364c04c88249b7b11c731c0896087359457dbd
linh-pham-nz <linh.pham.nz@gmail.com>
2019-11-15 13:17:43 +1300

    make button on home page link that redirects to random beer page

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Welcome = () => {
     return(
@@ -6,7 +7,7 @@ const Welcome = () => {
             <h1>Welcome to the BrewDog Beers page!</h1>
             <p>Click on the links above to find out more about individual beers.</p>
             <p>Click this button to learn about a random beer!</p>
-            <button id="beerButton">Cheers</button>
+            <Link to="/random-beer"><button id="beerButton">Cheers</button></Link>
         </React.Fragment>
     ) 
 }
