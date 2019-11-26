6a01d4d2bb1ff6d6b0aa66428b6115597b403ada
linh-pham-nz <linh.pham.nz@gmail.com>
2019-11-26 13:11:09 +1300

    render beer info in RandomBeer component

--- client/components/RandomBeer.jsx
+++ client/components/RandomBeer.jsx
@@ -22,11 +22,43 @@ class RandomBeer extends React.Component {
     }
 
     render() {
-        console.log(this.state.beer)
         return(
             <React.Fragment>
-                <p>Click this button to learn about a random beer!</p>
-                <button id="beerButton">Cheers</button>
+                <div>
+                    <p>Click this button to learn about a random beer!</p>
+                    <button id="beerButton">Cheers</button>
+                    <br></br>
+                </div>
+                <div>
+                    <div className="row">
+                        <div className="col-xl-2 col-sm-4">
+                            <img 
+                                id="chosenBeerImg"
+                                width="120" 
+                                src={this.state.beer.image_url === null 
+                                    ? "defaultPlaceholder.png" 
+                                    : this.state.beer.image_url}>
+                            </img>
+                        </div>
+                        <div className="col-xl-10 col-sm-8">
+                            <h2>{this.state.beer.name}</h2>
+                            <dl>
+                                <dt>Tagline:</dt>
+                                <dd>{this.state.beer.tagline}</dd>
+                                <dt>Description:</dt>
+                                <dd>{this.state.beer.description}</dd>
+                                <dt>ABV:</dt>
+                                <dd>{this.state.beer.abv}</dd>
+                                <dt>First brewed:</dt>
+                                <dd>{this.state.beer.first_brewed}</dd>
+                                <dt>Food pairing:</dt>
+                                {this.state.food_pairing.map((foods, i) => (
+                                    <dd id="food-text" key={i}>{foods}</dd>
+                                ))}
+                            </dl>
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         ) 
     }
