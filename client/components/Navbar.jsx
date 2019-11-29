0fd1c4a64b754f7cec23d52bf66c6878f2798007
linh-pham-nz <linh.pham.nz@gmail.com>
2019-11-29 18:06:35 +1300

    change Navbar background color

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <React.Fragment>
-            <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
+            <nav className="navbar navbar-expand-md navbar-light sticky-top">
                 <div className="container-fluid">
                     <div className="row">
                         <div className="d-flex">
