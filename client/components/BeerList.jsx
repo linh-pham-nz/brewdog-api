d0141d2e95c5083b66a9218b40f8a9bfd3f23372
linh-pham-nz <linh.pham.nz@gmail.com>
2019-12-02 14:12:59 +1300

    refactor beerSortForm into new component

--- client/components/BeerList.jsx
+++ client/components/BeerList.jsx
@@ -55,46 +55,7 @@ class BeerList extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <div>
-          <form>
-            <label htmlFor="sortBeerOptions">Sort beers: </label>
-            <select id="sortBeerOptions">
-                <option
-                  value="AlphabeticalA-Z">
-                    Alphabetical A-Z
-                </option>
-                <option
-                  value="AlphabeticalZ-A">
-                  Alphabetical Z-A
-                </option>
-                <option
-                  value="AbvAscending">
-                    ABV Ascending
-                </option>
-                <option
-                  value="AbvAscending">
-                    ABV Descending
-                </option>
-                <option
-                  value="IbuAscending">
-                    IBU Ascending
-                </option>
-                <option
-                  value="IbuDescending">
-                    IBU Descending
-                </option>
-                <option
-                  value="brewedOldest">
-                    Date First Brewed Oldest
-                  </option>
-                <option
-                  value="brewedLatest">
-                    Date First Brewed Latest
-                </option>
-            </select>
-            <input type="submit" value="Cheers" />
-          </form>
-        </div>
+        <BeerSortForm />
         <div className="row">
           {this.state.beers.map((beer, i) => {
             return (
--- client/components/BeerSortForm.jsx
+++ client/components/BeerSortForm.jsx
@@ -1,2 +1,48 @@
 import React from 'react'
 
+const BeerSortForm = () => {
+    return (
+        <React.Fragment>
+            <form>
+                <label htmlFor="sortBeerOptions">Sort beers: </label>
+                <select id="sortBeerOptions">
+                    <option
+                    value="AlphabeticalA-Z">
+                        Alphabetical A-Z
+                    </option>
+                    <option
+                    value="AlphabeticalZ-A">
+                    Alphabetical Z-A
+                    </option>
+                    <option
+                    value="AbvAscending">
+                        ABV Ascending
+                    </option>
+                    <option
+                    value="AbvAscending">
+                        ABV Descending
+                    </option>
+                    <option
+                    value="IbuAscending">
+                        IBU Ascending
+                    </option>
+                    <option
+                    value="IbuDescending">
+                        IBU Descending
+                    </option>
+                    <option
+                    value="brewedOldest">
+                        Date First Brewed Oldest
+                    </option>
+                    <option
+                    value="brewedLatest">
+                        Date First Brewed Latest
+                    </option>
+                </select>
+                <input type="submit" value="Cheers" />
+          </form>
+        </React.Fragment>
+    )
+}
+
+export default BeerSortForm
