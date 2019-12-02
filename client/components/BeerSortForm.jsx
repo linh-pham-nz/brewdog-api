d28ee2c455afa5bdf7630e012b7e3973d6c108f2
linh-pham-nz <linh.pham.nz@gmail.com>
2019-12-02 14:30:50 +1300

    WIP - dropdown - reformat BeerSortForm code

--- client/components/BeerSortForm.jsx
+++ client/components/BeerSortForm.jsx
@@ -1,48 +1,34 @@
 import React from 'react'
 
-const BeerSortForm = () => {
-    return (
-        <React.Fragment>
-            <form>
-                <label htmlFor="sortBeerOptions">Sort beers: </label>
-                <select id="sortBeerOptions">
-                    <option
-                    value="AlphabeticalA-Z">
-                        Alphabetical A-Z
-                    </option>
-                    <option
-                    value="AlphabeticalZ-A">
-                    Alphabetical Z-A
-                    </option>
-                    <option
-                    value="AbvAscending">
-                        ABV Ascending
-                    </option>
-                    <option
-                    value="AbvAscending">
-                        ABV Descending
-                    </option>
-                    <option
-                    value="IbuAscending">
-                        IBU Ascending
-                    </option>
-                    <option
-                    value="IbuDescending">
-                        IBU Descending
-                    </option>
-                    <option
-                    value="brewedOldest">
-                        Date First Brewed Oldest
-                    </option>
-                    <option
-                    value="brewedLatest">
-                        Date First Brewed Latest
-                    </option>
-                </select>
-                <input type="submit" value="Cheers" />
-          </form>
-        </React.Fragment>
-    )
+class BeerSortForm extends React.Component {
+    handleChange = (e) => {
+        const value = e.target.value
+        console.log(value)
+        // this.setState({
+
+        // })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <form>
+                    <label htmlFor="sortBeerOptions">Sort beers: </label>
+                    <select id="sortBeerOptions" onChange={this.handleChange}>
+                        <option value="alphabeticalA-Z">Alphabetical A-Z</option>
+                        <option value="alphabeticalZ-A">Alphabetical Z-A</option>
+                        <option value="abvAscending">ABV Ascending</option>
+                        <option value="abvAscending">ABV Descending</option>
+                        <option value="ibuAscending">IBU Ascending</option>
+                        <option value="ibuDescending">IBU Descending</option>
+                        <option value="brewedOldest">Date First Brewed Oldest</option>
+                        <option value="brewedLatest">Date First Brewed Latest</option>
+                    </select>
+                    <input type="submit" value="Cheers" />
+            </form>
+            </React.Fragment>
+        )
+    }
 }
 
 export default BeerSortForm
